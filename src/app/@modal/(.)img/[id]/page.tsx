f2efd332f069f533965dc9c98c920ceb463ad758
akash <akash329@protonmail.com>
2024-05-07 19:00:55 +0530

    fixed dialog full screen, made component for large view

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,7 +1,16 @@
+import FullPageImageView from "~/components/full-image-page";
+import { Modal } from "./modal";
+
 export default function PhotoModal({
   params: { id: photoId },
 }: {
   params: { id: string };
 }) {
-  return <div>{photoId}</div>;
+  const idAsNumber = Number(photoId);
+  if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
+  return (
+    <Modal>
+      <FullPageImageView id={idAsNumber} />
+    </Modal>
+  );
 }
--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,13 +1,11 @@
-import { getImage } from "~/server/queries";
+import FullPageImageView from "~/components/full-image-page";
 
-export default async function PhotoModal({
+export default function PhotoPage({
   params: { id: photoId },
 }: {
   params: { id: string };
 }) {
-
-    const idAsNumber = Number(photoId);
-    if(Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
-  const image = await getImage(idAsNumber);
-  return <div><img src={image.url} className="w-96"/></div>;
+  const idAsNumber = Number(photoId);
+  if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
+  return <FullPageImageView id={idAsNumber} />;
 }
