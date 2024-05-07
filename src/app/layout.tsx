c8b0e0c3b17e3049d8fe9e986ea8c0c52f145b6e
akash <akash329@protonmail.com>
2024-05-07 20:22:26 +0530

    move to grid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,12 +38,12 @@ export default function RootLayout({
            */
           routerConfig={extractRouterConfig(ourFileRouter)}
         />
-        <body className={`font-sans ${inter.variable} flex-col gap-4`}>
-          <div>
+        <body className={`font-sans ${inter.variable}`}>
+          <div className="grid h-screen grid-rows-[auto,1fr]">
             <TopNav />
-            {children}
-            {modal}
+            <main className="overflow-y-scroll">{children}</main>
           </div>
+          {modal}
           <div id="modal-root" />
         </body>
       </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,9 @@ export default async function HomePage() {
     const images = await getMyImages();
 
     return (
-      <div className="flex flex-wrap justify-center gap-4">
-        {images.map((image) => (
-          <div key={image.id} className="flex w-48 h-48 flex-col">
+      <div className="flex flex-wrap justify-center gap-4 p-4">
+        {[...images, ...images, ...images, ...images].map((image) => (
+          <div key={image.id} className="flex h-48 w-48 flex-col">
             <Link href={`img/${image.id}`}>
               <Image
                 src={image.url}
