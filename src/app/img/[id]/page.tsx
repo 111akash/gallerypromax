23383e78762aa1e841c669efec968da93e551cfd
akash <akash329@protonmail.com>
2024-05-09 00:31:25 +0530

    Analytics on delete button

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,4 +1,4 @@
-import FullPageImageView from "~/components/full-image-page";
+import { FullPageImageView } from "~/common/full-page-image-view";
 
 export default function PhotoPage({
   params: { id: photoId },
@@ -7,5 +7,5 @@ export default function PhotoPage({
 }) {
   const idAsNumber = Number(photoId);
   if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
-  return <FullPageImageView id={idAsNumber} />;
+  return <FullPageImageView photoId={photoId} />;
 }
